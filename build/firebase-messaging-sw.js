501cbf0a8388fb232e7608f1f8f48ae41327324b
srahmed <srehanpk@gmail.com>
2020-09-26 19:37:49 +0500

    add build

---
+++ build/firebase-messaging-sw.js
@@ -0,0 +1,18 @@
+importScripts ("https://www.gstatic.com/firebasejs/7.21.0/firebase-app.js");
+importScripts ("https://www.gstatic.com/firebasejs/7.21.0/firebase-messaging.js");
+
+
+
+
+  firebase.initializeApp(  {
+
+    apiKey: "AIzaSyBtBAouj6eNxLQPOrPrJdSqOw16SpvaEGY",
+    authDomain: "fir-messaging-1da67.firebaseapp.com",
+    databaseURL: "https://fir-messaging-1da67.firebaseio.com",
+    projectId: "fir-messaging-1da67",
+    storageBucket: "fir-messaging-1da67.appspot.com",
+    messagingSenderId: "882371787690",
+    appId: "1:882371787690:web:f17f1026700f80263dc9d3"
+  });
+
+ firebase.messaging();
---
+++ build/precache-manifest.73a053d25f451dd38e490f81072eb259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86405c2b24a77e9f531db2c886678e7a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eef0c2f8897bed29fa13",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b5f37b90c04b4c24d4a5",
+    "url": "/static/js/2.24e90629.chunk.js"
+  },
+  {
+    "revision": "27bbadeef7d02efcaae5fffc16c6b291",
+    "url": "/static/js/2.24e90629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eef0c2f8897bed29fa13",
+    "url": "/static/js/main.2ceffb6e.chunk.js"
+  },
+  {
+    "revision": "cb90df1b9ce3d2e6e39d",
+    "url": "/static/js/runtime-main.b4af7dac.js"
+  }
+]);
